f106b78980c7c3afb33ce09d338c91660322ee0a
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-10 13:36:48 +0900

    feat : Category 관련 테스트 추가

---
+++ src/test/java/domain/CategoryTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+	@Test
+	void isChickenTest() {
+		assertThat(Category.CHICKEN.isChicken()).isTrue();
+	}
+
+	@Test
+	void isNotChickenTest() {
+		assertThat(Category.BEVERAGE.isChicken()).isFalse();
+	}
+}
